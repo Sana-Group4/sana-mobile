4fa600435775a96c5e134ffef9f41e046ae86dfd
Mo Babiker <mobabiker127@gmail.com>
2026-03-04 00:34:27 +0000

    small login routing change

--- app/index.tsx
+++ app/index.tsx
@@ -44,7 +44,7 @@ export default function Login() {
 
       if (response.ok) {
         Alert.alert("Success", "Logged in successfully");
-        router.replace("/homepage"); // redirect to main app
+        router.replace("/client_home"); // redirect to main app
       } else {
         Alert.alert("Login failed", JSON.stringify(data));
       }
